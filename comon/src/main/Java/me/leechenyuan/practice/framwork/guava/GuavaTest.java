5d77ac3ff7c27eceb5489bf501bb9556e1daa87f
LeeChenYuan <463144199@qq.com>
2016-05-10 19:13:14 +0800

    init

---
+++ comon/src/main/Java/me/leechenyuan/practice/framwork/guava/GuavaTest.java
@@ -0,0 +1,14 @@
+package me.leechenyuan.practice.framwork.guava;
+
+import com.google.common.base.Optional;
+import org.junit.Test;
+
+
+/**
+ * Created by lideda on 2015/11/11.
+ */
+public class GuavaTest {
+    @Test
+    public void test(){
+    }
+}
